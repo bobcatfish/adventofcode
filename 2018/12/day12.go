752422bc2b2bf0b3cbe2c0207afac7606b1635ef
christie <bobcatfish@gmail.com>
2018-12-15 15:04:15 -0800

    Use bits more
    There is less string manipulation now, but it's still way too slow!

--- 2018/12/day12.go
+++ 2018/12/day12.go
@@ -10,60 +10,138 @@ import (
 
 const (
 	numGen = 20
-	plant  = '#'
-	empty  = '.'
+	//numGen = 50000000000
+	plant = '#'
+	empty = '.'
 )
 
-func getInitialState(s string) (string, error) {
+func getInitialState(s string) ([]byte, error) {
 	r, err := regexp.Compile("initial state: (.*)")
 	if err != nil {
-		return "", fmt.Errorf("couldn't compile initial state regex: %v", err)
+		return []byte{}, fmt.Errorf("couldn't compile initial state regex: %v", err)
 	}
 
 	m := r.FindStringSubmatch(s)
 	if len(m) != 2 {
-		return "", fmt.Errorf("didn't find expected values in %q", s)
+		return []byte{}, fmt.Errorf("didn't find expected values in %q", s)
 	}
 
-	return m[1], nil
+	state := ".." + m[1] + ".."
+	stateBytes := []byte{}
+	for i := 0; i < len(state); i++ {
+		var s string
+		if i == 0 {
+			s = ".."
+			s = s + state[i:i+3]
+		} else if i == 1 {
+			s = "."
+			s = s + state[i-1:i+3]
+		} else if i == len(state)-2 {
+			s = state[i-2 : i+2]
+			s = s + "."
+		} else if i == len(state)-1 {
+			s = state[i-2 : i+1]
+			s = s + ".."
+		} else {
+			s = state[i-2 : i+3]
+		}
+		stateBytes = append(stateBytes, patternToByte(s))
+	}
+
+	return stateBytes, nil
 }
 
-func getSpread(s string) (string, rune, error) {
+func patternToByte(p string) byte {
+	b := byte(0)
+	for i := 0; i < len(p); i++ {
+		if p[i] == plant {
+			b |= byte(1 << uint(i))
+		}
+	}
+	return b
+}
+
+func bytesToByte(p []byte) byte {
+	b := byte(0)
+	for i := 0; i < len(p); i++ {
+		b |= byte(p[i] << uint(i))
+	}
+	return b
+}
+
+func getSpread(s string) (byte, byte, error) {
 	r, err := regexp.Compile("(.*) => (.*)")
 	if err != nil {
-		return "", ' ', fmt.Errorf("couldn't compile regex: %v", err)
+		return 0, 0, fmt.Errorf("couldn't compile regex: %v", err)
 	}
 
 	m := r.FindStringSubmatch(s)
 	if len(m) != 3 {
-		return "", ' ', fmt.Errorf("didn't find expected values in %q", s)
+		return 0, 0, fmt.Errorf("didn't find expected values in %q", s)
 	}
 
-	return m[1], rune(m[2][0]), nil
+	newVal := byte(0)
+	if m[2][0] == plant {
+		newVal = 1
+	}
+
+	return patternToByte(m[1]), newVal, nil
 }
 
-func count(s string, start int) int {
+func count(s []byte, start int) int {
 	count := 0
 	for i, r := range s {
-		if r == plant {
+		if r == 1 {
 			count += start + i
 		}
 	}
 	return count
 }
 
-func mutate(state string, spreads map[string]rune) string {
-	state = fmt.Sprintf("%c%c%c%c%s%c%c%c%c", empty, empty, empty, empty, state, empty, empty, empty, empty)
-	newState := ""
-	for i := 2; i < len(state)-2; i++ {
-		s := state[i-2 : i+3]
-		if r, ok := spreads[s]; ok != false {
-			newState += string(r)
+func expandState(state []byte) []byte {
+	stateBytes := []byte{}
+	for i := 0; i < len(state); i++ {
+		var s []byte
+		if i == 0 {
+			s = []byte{0, 0}
+			s = append(s, state[i:i+3]...)
+		} else if i == 1 {
+			s = []byte{0}
+			s = append(s, state[i-1:i+3]...)
+		} else if i == len(state)-2 {
+			s = state[i-2 : i+2]
+			s = append(s, 0)
+		} else if i == len(state)-1 {
+			s = state[i-2 : i+1]
+			s = append(s, 0, 0)
 		} else {
-			newState += string(r)
+			s = state[i-2 : i+3]
+		}
+		stateBytes = append(stateBytes, bytesToByte(s))
+	}
+	return stateBytes
+}
+
+func mutate(state []byte, spreads map[byte]byte) ([]byte, []byte) {
+	newState := []byte{0, 0}
+
+	for i := 0; i < len(state); i++ {
+		newState = append(newState, spreads[state[i]])
+	}
+	newState = append(newState, 0, 0)
+
+	return newState, expandState(newState)
+}
+
+func displayBytes(b []byte) {
+	for i := 0; i < len(b); i++ {
+		if b[i] == byte(1) {
+			fmt.Printf("#")
+		} else {
+			fmt.Printf(".")
 		}
 	}
-	return newState
+	fmt.Println()
 }
 
 func main() {
@@ -83,11 +161,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't get initial state: %v", err)
 	}
-	fmt.Println(initial)
 
-	spreads := map[string]rune{}
+	spreads := map[byte]byte{}
 	for i := 2; i < len(vals); i++ {
-		// TODO
 		s, r, err := getSpread(vals[i])
 		if err != nil {
 			log.Fatalf("Couldn't get spread from %q: %v", vals[i], err)
@@ -96,13 +172,15 @@ func main() {
 	}
 
 	state := initial
-	start := 0
+
+	start := -2
+	var n []byte
 	for i := 0; i < numGen; i++ {
-		state = mutate(state, spreads)
-		fmt.Println(state)
+		n, state = mutate(state, spreads)
+		//displayBytes(n)
 		start -= 2
 	}
 
 	fmt.Println("start is", start)
-	fmt.Println(count(state, start))
+	fmt.Println(count(n, start))
 }
