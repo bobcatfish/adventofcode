2859376e5199f4bc3ed14ec545fea7bd9e68be10
christie <bobcatfish@gmail.com>
2023-12-01 21:54:43 -0800

    Day 2 part 2
    I really need to use a more recent verion of python 😅

--- 2023/2/main.py
+++ 2023/2/main.py
@@ -3,7 +3,7 @@ import collections
 import re
 
 
-Game = collections.namedtuple("Game", ["game_id", "rounds"])
+Game = collections.namedtuple("Game", ["game_id", "rounds", "mins"])
 
 bag = {"red": 12, "green": 13, "blue": 14}
 
@@ -26,7 +26,16 @@ def parse_game(line):
             showings[color] = int(num)
         rounds.append(showings)
 
-    return Game(game_id=game_id, rounds=rounds)
+    return Game(game_id=game_id, rounds=rounds, mins=get_mins(rounds))
+
+
+def get_mins(rounds):
+    min_bag = {"red": 0, "green": 0, "blue": 0}
+    for r in rounds:
+        for color, num in r.items():
+            if num > min_bag[color]:
+                min_bag[color] = num
+    return min_bag
 
 
 def valid_game(game):
@@ -37,12 +46,7 @@ def valid_game(game):
     return True
 
 
-if __name__ == "__main__":
-    games = []
-    with open("input.txt") as f:
-        for line in f.readlines():
-            games.append(parse_game(line))
-
+def valid_games(games):
     valid = []
     for game in games:
         if valid_game(game):
@@ -53,3 +57,20 @@ if __name__ == "__main__":
         s += game.game_id
 
     print(s)
+
+
+if __name__ == "__main__":
+    games = []
+    with open("input.txt") as f:
+        for line in f.readlines():
+            games.append(parse_game(line))
+
+    valid_games(games)
+
+    s = 0
+    for game in games:
+        s += game.mins["red"] * game.mins["green"] * game.mins["blue"]
+    print(s)
+
+
+
