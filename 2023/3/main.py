3a1fbaaf4ee8cf53b92ca2ec425394155344b7fb
christie <bobcatfish@gmail.com>
2023-12-03 20:40:59 -0800

    Day 3 part 2: icky but it gets the job done

--- 2023/3/main.py
+++ 2023/3/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import math
 
 
@@ -15,15 +16,14 @@ dirs = [
 ]
 
 
-
 def ispart(schem, y, x):
     for d in dirs:
         dy, dx = y + d[0], x + d[1]
         if (dy >= 0 and dy < len(schem) and dx >= 0 and dx < len(schem[0])):
             val = schem[dy][dx]
             if not val.isdigit() and val != ".":
-                return True
-    return False
+                return val, (dy, dx)
+    return None, None
 
 
 def get_num(nums):
@@ -35,30 +35,43 @@ def get_num(nums):
 
 def get_parts(schem):
     num = []
-    part = False
+    part = None
     parts = []
+    gears = collections.defaultdict(list)
     for y in range(0, len(schem)):
         for x in range(0, len(schem[y])):
             if schem[y][x].isdigit():
                 num.insert(0, int(schem[y][x]))
                 if not part:
-                    part = ispart(schem, y, x)
+                    part, loc = ispart(schem, y, x)
+                    if part == "*":
+                        part = loc
             else:
                 if part:
                     parts.append(get_num(num))
+                    if len(part) > 1:
+                        gears[part].append(parts[-1])
                 num = []
                 part = False
         if part:
             parts.append(get_num(num))
+            if len(part) > 1:
+                gears[part].append(parts[-1])
             num = []
             part = False
-    return parts
-
+    return parts, gears
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         schem = [line.rstrip() for line in f.readlines()]
-        parts = get_parts(schem)
+
+        parts, gears = get_parts(schem)
         print(sum(parts))
+
+        v = 0
+        for loc, vals in gears.items():
+            if len(vals) == 2:
+                v += vals[0] * vals[1]
+        print(v)
 
