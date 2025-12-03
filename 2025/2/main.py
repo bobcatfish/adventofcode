bfe29c10dcd0443394705cd92e87b5e09b0f4c85
christie <bobcatfish@gmail.com>
2025-12-02 21:21:01 -0800

    Day 2: part 2 well i made it through a part 2!
    I wrote some gnarly logic, im so glad I didn't have to debug it XD

--- 2025/2/main.py
+++ 2025/2/main.py
@@ -7,6 +7,19 @@ def invalid(n):
     return n[:len(n)//2] == n[len(n)//2:]
 
 
+def invalid2(n):
+    for i in range(1, len(n)//2+1):
+        seq = n[:i]
+        found = True
+        for j in range(i, len(n), len(seq)):
+            if seq != n[j:j+len(seq)]:
+                found = False
+                break
+        if found:
+            return True
+    return False
+
+
 if __name__ == "__main__":
     ids = []
     with open("input.txt") as f:
@@ -15,13 +28,16 @@ if __name__ == "__main__":
                 start, end = group.split('-')
                 ids.append((int(start), int(end)))
 
-    invalids = []
+    invalids, invalids2 = [], []
     for (start, end) in ids:
         while start <= end:
             if invalid(str(start)):
                 invalids.append(start)
+            if invalid2(str(start)):
+                invalids2.append(start)
             start += 1
 
     print(sum(invalids))
+    print(sum(invalids2))
 
 
