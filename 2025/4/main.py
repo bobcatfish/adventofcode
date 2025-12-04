c4d767b1dff84c1d3eea006bc1f16a4fc23ea3bf
christie <bobcatfish@gmail.com>
2025-12-03 21:18:55 -0800

    Day 4 p1: Am I just doing part 1s now? maybe

---
+++ 2025/4/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import collections
+
+
+Point = collections.namedtuple('Point', ['y', 'x'])
+
+Left = Point(0, -1)
+Right = Point(0, 1)
+Up = Point(-1, 0)
+Down = Point(1, 0)
+UpperLeft = Point(-1, -1)
+UpperRight = Point(-1, 1)
+LowerLeft = Point(1, -1)
+LowerRight = Point(1, 1)
+
+Neighbors = [
+    Left,
+    Right,
+    Up,
+    Down,
+    UpperLeft,
+    UpperRight,
+    LowerLeft,
+    LowerRight
+]
+
+ROLL = '@'
+MAX = 4
+
+
+def is_roll(loc, grid):
+    return grid[loc.y][loc.x] == ROLL
+
+
+def in_grid(nn, grid):
+    return (nn.y >= 0 and nn.y < len(grid) and
+            nn.x >= 0 and nn.x < len(grid[0]))
+
+
+def num_rolls(loc, grid):
+    count = 0
+    for n in Neighbors:
+        nn = Point(loc.y + n.y, loc.x + n.x)
+        if in_grid(nn, grid):
+            if is_roll(nn, grid):
+                count += 1
+    return count
+
+
+if __name__ == "__main__":
+    grid = []
+    with open("input.txt") as f:
+        for line in f.readlines():
+            grid.append(line)
+
+
+    accessible = []
+    for y in range(0, len(grid)):
+        for x in range (0, len(grid[0])):
+            p = Point(y, x)
+            if is_roll(p, grid):
+                c = num_rolls(p, grid)
+                if c < 4:
+                    accessible.append(p)
+
+    print(len(accessible))
+
+
